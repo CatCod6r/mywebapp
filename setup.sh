0518ed0b5fa7826bceae85172b6bb5faadaa93b0
CatCod6r <ncebrinskij@gmail.com>
2026-05-25 11:19:14 +0300

    make manula commands for runner setup

--- setup.sh
+++ setup.sh
@@ -31,11 +31,3 @@ echo 'Контейнери успішно запущені!'
 
 echo 'даєм права веріфаю'
 chmod +x ./verify.sh 
-
-echo 'Запускаєм ранер'
-mkdir actions-runner && cd actions-runner
-curl -o actions-runner-linux-x64-2.334.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.334.0/actions-runner-linux-x64-2.334.0.tar.gz
-echo "048024cd2c848eb6f14d5646d56c13a4def2ae7ee3ad12122bee960c56f3d271  actions-runner-linux-x64-2.334.0.tar.gz" | shasum -a 256 -c
-tar xzf ./actions-runner-linux-x64-2.334.0.tar.gz
-./config.sh --url https://github.com/CatCod6r/mywebapp --token TOKEN
-./run.sh
